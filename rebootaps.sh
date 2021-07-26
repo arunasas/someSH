68690be544c6c4404e03cd9b30fe6f9054dece13
arunasas <57219076+arunasas@users.noreply.github.com>
2021-07-26 22:39:16 +0300

    Add files via upload

--- rebootaps.sh
+++ rebootaps.sh
@@ -22,21 +22,44 @@ USER="admin"
 PASS0="/root/sshpasswd0"
 PASS1="/root/sshpasswd1"
 PASS2="/root/sshpasswd2"
+PASSTEST="sshpasswdtest"
 
-IPBASE=$1
-IPSTART=$2
-IPEND=$3
+IPBASE=$2
+IPSTART=$3
+IPEND=$4
+
+selectPasswdFile ()
+{
+	case "$( tr '[:upper:]' '[:lower:]' <<< "$1" )" in 
+		pass0)
+			PASS=$PASS0
+			;;
+		pass1)
+			PASS=$PASS1
+			;;
+		pass2)
+			PASS=$PASS2
+			;;
+		passtest)
+			PASS=$PASSTEST
+			;;
+		*)
+			echo "No such $1 sshpasswdfile. Quitting."
+			exit 1
+			;;
+	esac
+}
 
 reboot ()
 {
     IP=$1
-    $SSHPASS -f $PASS0 $SSH $USER@$IP $SSHOPTSNFT 'uptime'
+    $SSHPASS -f $PASS $SSH $USER@$IP $SSHOPTSAPC 'uname -a'
 }
 
 delayCounter ()
 {
     echo -n "Next reboot in [s]: "
-    n=$1a
+    n=$1
     while [ $n -ge 10 ]
     do
         echo -n "$n "
@@ -48,10 +71,13 @@ delayCounter ()
 
 showUsage ()
 {
-	echo "Usage example: $0 192.168.8. 10 15"
+	echo "Usage example: $0 sshpasswdfile x.x.x. x x"
+	echo "Usage example: $0 sshpasswdfile 192.168.8. 10 15"
 }
 
-showUsage
+#showUsage
+
+selectPasswdFile $1
 
 for ((i=$IPSTART; i<=$IPEND; i++))
     do
@@ -60,5 +86,5 @@ for ((i=$IPSTART; i<=$IPEND; i++))
     [ $? -eq 0 ] && echo "$IPBASE$i sent reboot command" || 
     echo "$IPBASE$i failed to send reboot command"
     
-    delayCounter 40
+    #delayCounter 40
     done
